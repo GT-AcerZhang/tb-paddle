89915e72290857e79b4fdb11710de25558de3b37
David Lin <xiaolin_2292@qq.com>
2019-08-26 11:12:23 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,26 +3,22 @@ set -ex
 
 DESIRED_PROTO_VERSION="3.6.1"
 
-PROTOC_BIN=`which protoc`
-
-if [ -z ${PROTOC_BIN} ]; then
-  # Download and use the latest version of protoc.
-    if [ "$(uname)" == "Darwin" ]; then
-    PROTOC_ZIP="protoc-"$DESIRED_PROTO_VERSION"-osx-x86_64.zip"
-  else
-    PROTOC_ZIP="protoc-"$DESIRED_PROTO_VERSION"-linux-x86_64.zip"
-  fi
-  WGET_BIN=`which wget`
-  if [[ ! -z ${WGET_BIN} ]]; then
-    ${WGET_BIN} https://github.com/protocolbuffers/protobuf/releases/download/v"$DESIRED_PROTO_VERSION"/${PROTOC_ZIP}
-    rm -rf protoc
-    python -c "import zipfile; zipfile.ZipFile('"${PROTOC_ZIP}"','r').extractall('protoc')"
-    PROTOC_BIN=protoc/bin/protoc
-    chmod +x ${PROTOC_BIN}
-  fi
+# Download and use the latest version of protoc.
+if [ "$(uname)" == "Darwin" ]; then
+  PROTOC_ZIP="protoc-"$DESIRED_PROTO_VERSION"-osx-x86_64.zip"
+else
+  PROTOC_ZIP="protoc-"$DESIRED_PROTO_VERSION"-linux-x86_64.zip"
 fi
 
-# Regenerate
+WGET_BIN=`which wget`
+if [[ ! -z ${WGET_BIN} ]]; then
+  ${WGET_BIN} https://github.com/protocolbuffers/protobuf/releases/download/v"$DESIRED_PROTO_VERSION"/${PROTOC_ZIP}
+  rm -rf protoc
+  python -c "import zipfile; zipfile.ZipFile('"${PROTOC_ZIP}"','r').extractall('protoc')"
+  PROTOC_BIN=protoc/bin/protoc
+  chmod +x ${PROTOC_BIN}
+fi
+
 if [[ -n ${PROTOC_BIN} ]]; then
   echo "Delete all existing Python protobuf (*_pb2.py) output"
   rm -rf tb_paddle/proto/*_pb2.py
