55156581177a9c3ff5796ad93618b57d9b80c36e
linshuliang <xiaolin_2292@qq.com>
2019-12-26 19:59:49 +0800

    modify the if-else of tb_paddle/record_writer.py

--- tb_paddle/record_writer.py
+++ tb_paddle/record_writer.py
@@ -11,19 +11,19 @@ def directory_check(log_dir):
     :param log_dir: The directory to store log files.
     :type log_dir: str
     """
-    if os.path.exists(log_dir) and os.path.isfile(log_dir):
-        logger.error("{} is already exists, however, \
-            it is not a directory.".format(log_dir))
-        exit()
- 
-    if not os.path.exists(log_dir):
+    if os.path.exists(log_dir):
+        if os.path.isfile(log_dir):
+            logger.error("{} is already exists, however, \
+                it is not a directory.".format(log_dir))
+            exit()
+    else: 
         os.makedirs(log_dir)
      
      
 class RecordWriter(object):
-    def __init__(self, path):
-        self.path = path
-        self._writer = open(path, 'wb')
+    def __init__(self, file_path):
+        self.file_path= file_path
+        self._writer = open(self.file_path, 'wb')
      
     def write(self, data):
         w = self._writer.write
